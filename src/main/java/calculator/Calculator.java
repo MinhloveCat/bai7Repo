9f581fc26f3bf0d4f410421274a1ba6e8244988e
MinhloveCat <efriendlybavi@gmail.com>
2026-05-07 18:27:24 +0700

    final commit plssssss1

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,16 +1,18 @@
-package calulator;
+package calculator;
 
 /**
  * Simple calculator class.
  */
-
 public final class Calculator {
 
-  public static final int THE_FIRST_INTEGER = 10;
-  public static final int THE_SECOND_INTEGER = 20;
+  /** First integer. */
+  private static final int THE_FIRST_INTEGER = 10;
+
+  /** Second integer. */
+  private static final int THE_SECOND_INTEGER = 20;
 
   /**
-   * Add two integers.
+   * Adds two integers.
    *
    * @param a first integer
    * @param b second integer
@@ -18,8 +20,11 @@ public final class Calculator {
    */
   public int add(final int a, final int b) {
     System.out.println("Thuc hien phep cong: " + a + " + " + b);
-    int result = a + b;
+
+    final int result = a + b;
+
     System.out.println("Ket qua: " + result);
+
     return result;
   }
 
@@ -29,8 +34,9 @@ public final class Calculator {
    * @param args command-line arguments
    */
   public static void main(final String[] args) {
-    final Calculator calc = new Calculator();
-    calc.add(THE_FIRST_INTEGER, THE_SECOND_INTEGER);
 
+    final Calculator calc = new Calculator();
+
+    calc.add(THE_FIRST_INTEGER, THE_SECOND_INTEGER);
   }
 }
